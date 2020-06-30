621db603c2351b7e19775e539de87809efcd2867
DiegoPrestesGit <diegoprestes.git@hotmail.com>
2020-06-30 13:44:49 -0300

    config test

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,37 +1,23 @@
 import { Router } from 'express'
 import { container } from 'tsyringe'
 
-import CreateUserService from '@modules/users/services/CreateUserService'
 import ensureAuthenticaded from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 import multer from 'multer'
 import uploadConfig from '@config/upload'
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService'
+import UsersController from '@modules/users/infra/http/controllers/UsersController'
+import UserAvatarController from '@modules/users/infra/http/controllers/UserAvatarController'
 
 const usersRouter = Router()
 const upload = multer(uploadConfig)
+const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
 
-usersRouter.post('/', async (request, response) => {
-  const { name, email, password } = request.body
+usersRouter.post('/', usersController.create)
 
-  const createUser = container.resolve(CreateUserService)
-  const user = await createUser.execute({ name, email, password })
-
-  delete user.password
-
-  return response.json(user)
-})
-
-usersRouter.patch('/avatar', ensureAuthenticaded, upload.single('avatar'), async (request, response) => {
-  const updateUserAvatar = container.resolve(UpdateUserAvatarService)
-
-  const user = await updateUserAvatar.execute({
-    user_id: request.user.id,
-    avatarFileName: request.file.filename
-  })
-
-  delete user.password
-
-  return response.json(user)
-})
+usersRouter.patch('/avatar',
+  ensureAuthenticaded,
+  upload.single('avatar'),
+  userAvatarController.update
+)
 
 export default usersRouter
