d582b49f94f904181b150bd43cb0fd9b9ed4c457
DiegoPrestesGit <diegoprestes.git@hotmail.com>
2020-07-18 15:29:28 -0300

    listando providers

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -3,9 +3,20 @@ import { uuid } from 'uuidv4'
 import User from '@modules/users/infra/typeorm/entities/User'
 import IUserRepository from '@modules/users/repositories/IUsersRepository'
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO'
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO'
 
 class FakeUsersRepository implements IUserRepository {
   private users: User[] = []
+
+  public async findAllProviders ({ except_user_id }: IFindAllProvidersDTO): Promise<User[]> {
+    let users = this.users
+
+    if (except_user_id) {
+      users = this.users.filter(user => user.id !== except_user_id)
+    }
+
+    return users
+  }
 
   public async findById (id: string): Promise<User | undefined> {
     const user = this.users.find(user => user.id === id)
