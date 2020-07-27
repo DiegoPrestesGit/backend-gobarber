ae772eac8611b70117f4631ff5da16dd00633f40
DiegoPrestesGit <diegoprestes.git@hotmail.com>
2020-07-27 09:12:01 -0300

    data validation

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { celebrate, Segments, Joi } from 'celebrate'
 
 import ForgotPasswordController from '@modules/users/infra/http/controllers/ForgotPasswordController'
 import ResetPasswordController from '@modules/users/infra/http/controllers/ResetPasswordController'
@@ -7,7 +8,23 @@ const sessionRouter = Router()
 const forgotPasswordController = new ForgotPasswordController()
 const resetPasswordController = new ResetPasswordController()
 
-sessionRouter.post('/forgot', forgotPasswordController.create)
-sessionRouter.post('/reset', resetPasswordController.create)
+sessionRouter.post('/forgot',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().required()
+    }
+  }),
+  forgotPasswordController.create
+)
+sessionRouter.post('/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password'))
+    }
+  }),
+  resetPasswordController.create
+)
 
 export default sessionRouter
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { celebrate, Segments, Joi } from 'celebrate'
 
 import ensureAuthenticaded from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 import multer from 'multer'
@@ -11,7 +12,16 @@ const upload = multer(uploadConfig)
 const usersController = new UsersController()
 const userAvatarController = new UserAvatarController()
 
-usersRouter.post('/', usersController.create)
+usersRouter.post('/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required()
+    }
+  }),
+  usersController.create
+)
 
 usersRouter.patch('/avatar',
   ensureAuthenticaded,
