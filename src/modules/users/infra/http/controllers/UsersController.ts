85c8f074afe20f46a2a43d5b217d11e1528e5709
DiegoPrestesGit <diegoprestes.git@hotmail.com>
2020-07-27 20:46:44 -0300

    forgot to save xd

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -2,7 +2,6 @@ import { Request, Response } from 'express'
 import { container } from 'tsyringe'
 import { classToClass } from 'class-transformer'
 
-// import AuthenticateUserService from '@modules/users/services/AuthenticateUserService'
 import CreateUserService from '@modules/users/services/CreateUserService'
 
 export default class UsersController {
