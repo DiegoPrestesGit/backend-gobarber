9a254667110ae0f03f9647d2452b820ba0940660
DiegoPrestesGit <diegoprestes.git@hotmail.com>
2020-07-29 14:51:37 -0300

    emailing

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses'
+
+  defaults: {
+    from: {
+      email: string
+      name: string
+    }
+  }
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'diego@dpsynth.com',
+      name: 'Diego Prestes'
+    }
+  }
+} as IMailConfig
