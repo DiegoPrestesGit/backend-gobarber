abfccd1b7ca4c580e39078ba9d5043a5c3bdf39d
DiegoPrestesGit <diegoprestes.git@hotmail.com>
2020-07-31 12:49:20 -0300

    going to the tests, cache working

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe'
 
 import AppError from '@shared/errors/AppError'
 import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider'
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider'
 
 import User from '@modules/users/infra/typeorm/entities/User'
 import IUsersRepository from '@modules/users/repositories/IUsersRepository'
@@ -19,7 +20,10 @@ class CreateUserService {
     private usersRepository: IUsersRepository,
 
     @inject('HashProvider')
-    private hashProvider: IHashProvider
+    private hashProvider: IHashProvider,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider
   ) {}
 
   public async execute ({ name, email, password }: IRequestDTO): Promise<User> {
@@ -36,6 +40,8 @@ class CreateUserService {
       email,
       password: hashedPass
     })
+
+    await this.cacheProvider.invalidatePrefix('providers-list')
 
     return user
   }
