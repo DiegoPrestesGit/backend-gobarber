3d6f61a82c97c21a7a39c4612420d6f23d9849c3
DiegoPrestesGit <diegoprestes.git@hotmail.com>
2020-07-31 09:38:31 -0300

    starting my cache

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,20 +1,52 @@
 import path from 'path'
 import crypto from 'crypto'
-import multer from 'multer'
+import multer, { StorageEngine } from 'multer'
 
 const tempFolder = path.resolve(__dirname, '..', '..', 'temp')
 
+interface IUploadConfig {
+  tempFolder: string
+  uploadsFolder: string
+  driver: 's3' | 'disk'
+
+  config: {
+    multer: {
+      storage: StorageEngine
+    }
+
+    disk: {}
+
+    aws: {
+      bucket: string
+    }
+  }
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tempFolder,
   uploadsFolder: path.resolve(tempFolder, 'uploads'),
   diretory: tempFolder,
-  storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'temp'),
-    filename (request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX')
-      const fileName = `${fileHash}-${file.originalname}`
 
-      return callback(null, fileName)
+  config: {
+    multer: {
+      storage: multer.diskStorage({
+        destination: path.resolve(__dirname, '..', '..', 'temp'),
+        filename (request, file, callback) {
+          const fileHash = crypto.randomBytes(10).toString('HEX')
+          const fileName = `${fileHash}-${file.originalname}`
+
+          return callback(null, fileName)
+        }
+      })
+    },
+
+    disk: {},
+
+    aws: {
+      bucket: 'gobarber-gostack2020'
     }
-  })
-}
+  }
+
+} as IUploadConfig
--- src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import path from 'path'
-
+import mime from 'mime'
 import aws, { S3 } from 'aws-sdk'
 import uploadConfig from '@config/upload'
 import IStorageProvider from '../models/IStorageProvider'
@@ -17,28 +17,31 @@ export default class DiskStorageProvider implements IStorageProvider {
   public async saveFile (file: string): Promise<string> {
     const originalPath = path.resolve(uploadConfig.tempFolder, file)
 
-    const fileContent = await fs.promises.readFile(originalPath, {
-      encoding: 'utf-8'
-    })
+    const fileContent = await fs.promises.readFile(originalPath)
+
+    const ContentType = mime.getType(originalPath)
+
+    if (!ContentType) {
+      throw new Error('File Not Found')
+    }
 
     await this.client.putObject({
-      Bucket: 'gobarber-gostack2020',
-      Key: __filename,
+      Bucket: uploadConfig.config.aws.bucket,
+      Key: file,
       ACL: 'public-read',
-      Body: fileContent
+      Body: fileContent,
+      ContentType
     }).promise()
+
+    await fs.promises.unlink(originalPath)
 
     return file
   }
 
   public async deleteFile (file: string): Promise<void> {
-    const filePath = path.resolve(uploadConfig.uploadsFolder, file)
-
-    try {
-      await fs.promises.stat(filePath)
-    } catch {
-    }
-
-    await fs.promises.unlink(filePath)
+    await this.client.deleteObject({
+      Bucket: uploadConfig.config.aws.bucket,
+      Key: file
+    }).promise()
   }
 }
